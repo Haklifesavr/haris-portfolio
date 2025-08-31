3e8f0865ee380491f4a2cb58a7c4402a2205e87b
Haris Ahmed khan <hak173129@gmail.com>
2025-08-31 13:58:37 +0500

    updated resume url

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -129,7 +129,7 @@ const Hero = () => {
                             })}>
                             View Projects
                         </Button>
-                        <a href="https://drive.google.com/file/d/1IcWfi9mCDlijy8lCO3cCSwgt2DKr7gRZ/view?usp=drivesdk" rel="noreferrer" target='_blank'>
+                        <a href="https://drive.google.com/file/d/1_FhQMhlXZ9PV1YBLTse8xkM1MMevOVy_/view?usp=sharing" rel="noreferrer" target='_blank'>
                             <Button
                                 className='b2'
                                 sx={{
